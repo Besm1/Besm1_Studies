71e732399401994d7c5c79a3e2b1a926cbaa2274
Besm1 <besm1@yandex.ru>
2024-07-19 19:45:15 +0300

    Tested and debugged

---
+++ homeWork2.py
@@ -0,0 +1,7 @@
+tasks_completed = 12        # 1. Количество выполненных ДЗ
+hours_spent     = 1.5       # 2. Количество затраченных часов
+course_title    = 'Python'  # 3. Название курса
+avg_task_time   = hours_spent / tasks_completed
+
+print('Курс:', course_title, ', всего задач:', tasks_completed, ', затрачено часов:' ,hours_spent
+      , ', среднее время выполнения', avg_task_time,'часа.')
