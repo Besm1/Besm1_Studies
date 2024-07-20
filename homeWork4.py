eac9f66064ed33f7b36d89f52cd0e21c08ea822d
Besm1 <besm1@yandex.ru>
2024-07-20 21:50:03 +0300

    Tested and debugged

---
+++ homeWork4.py
@@ -0,0 +1,15 @@
+# 2
+my_string = input("Give me a string: ")
+print('String entered: "' + my_string + '"')
+# 3.1
+print('Input string length:', len(my_string))
+# 3.2
+print('Upper case:', my_string.upper())
+# 3.3
+print('Lower case:', my_string.lower())
+# 3.4
+print('Whitespaces removed:', my_string.replace(' ', ''))
+# 3.5
+print('1st char:', my_string[0])
+# 3.5
+print('Last char:', my_string[-1:])    #[len(my_string)-1])
