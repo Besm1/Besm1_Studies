20ab85e3f043cd31ac113ad963d93e0a06e5b5e6
Besm1 <besm1@yandex.ru>
2024-07-23 23:19:35 +0300

    Tested and debugged

---
+++ module1hard.py
@@ -0,0 +1,16 @@
+# Initialize sequences
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+avg_grades = []
+
+# Convert unordered set to sorted ordered list
+student_l = sorted(list(students))
+print('Sorted list of students:', student_l)
+
+# Calculate AVG grades and put them into list
+for grade in grades: avg_grades.append(sum(grade)/len(grade))
+print('List of average grades:', avg_grades)
+
+# Pack two lists into dictionary
+grades_registry = dict(zip(student_l, avg_grades))
+print("Students' average grades:", grades_registry)
