023c81cdc3beef1e3da7f35c8c94b2cdfc807d05
Besm1 <besm1@yandex.ru>
2024-07-31 23:29:41 +0300

    Works well.

--- module3hard.py
+++ module3hard.py
@@ -3,7 +3,7 @@ def calculate_structure_sum(*args):
     if args[0] == None or not args[0]:  # Пустой аргумент или пустая коллекция
         return 0
     if isinstance(args[0], set) or isinstance(args[0], tuple) or isinstance(args[0], list):   # Колллекция -
-        lst = list(args[0])     # Преобразуем в сисок, т.к. у него есть метод .pop()
+        lst = list(args[0])     # Преобразуем в список, т.к. у него есть метод .pop()
                                 # будем откусывать и рассчитывать по одному элементу,
                                 # и прибавлять сумму оставшейся части списка
         return calculate_structure_sum(lst.pop()) + calculate_structure_sum(lst)
