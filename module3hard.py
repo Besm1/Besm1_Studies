f49bb3772508409752d7e14653b10db32d680eac
Besm1 <besm1@yandex.ru>
2024-07-31 23:30:37 +0300

    Works well.

--- module3hard.py
+++ module3hard.py
@@ -4,7 +4,7 @@ def calculate_structure_sum(*args):
         return 0
     if isinstance(args[0], set) or isinstance(args[0], tuple) or isinstance(args[0], list):   # Колллекция -
         lst = list(args[0])     # Преобразуем в список, т.к. у него есть метод .pop()
-                                # будем откусывать и рассчитывать по одному элементу,
+                                # будем выталкивать и рассчитывать по одному элементу,
                                 # и прибавлять сумму оставшейся части списка
         return calculate_structure_sum(lst.pop()) + calculate_structure_sum(lst)
     elif isinstance(args[0], dict):     # Словарь преобразуем в список кортежей и обработаем полученную коллекцию
