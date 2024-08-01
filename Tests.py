4a868ca0d8d25024cde449818daad22cf5761ef4
Besm1 <besm1@yandex.ru>
2024-08-01 14:32:53 +0300

    Works well.

--- Tests.py
+++ Tests.py
@@ -12,18 +12,21 @@
 # print(' ' in a)
 #
 #
+#
+# a = {'a':1, 'b':3, 'c':5}
+# print(a)
+# b = list(a.items())
+# print(b)
+# c={1,2,3,4,5}
+# print(c)
+# print(c.pop())
+# print(c)
+# d=[1,2,33,4,5,6]
+# print(d)
+# print(d.pop())
+# print(list(d))
+# print(list(a))
+# print(list(b))
 
-a = {'a':1, 'b':3, 'c':5}
-print(a)
-b = list(a.items())
-print(b)
-c={1,2,3,4,5}
-print(c)
-print(c.pop())
-print(c)
-d=[1,2,33,4,5,6]
-print(d)
-print(d.pop())
-print(list(d))
-print(list(a))
-print(list(b))
+for i in range (1, 2):
+    print(i)
