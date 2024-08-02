cb96dee68bc06e4ca03423d2706718004793e37c
Besm1 <besm1@yandex.ru>
2024-08-02 22:26:46 +0300

    Works well.

--- module_4_1.py
+++ module_4_1.py
@@ -1,6 +1,15 @@
 from true_math_4_1 import divide as true_divide
 from fake_math_4_1 import divide as fake_divide
 
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print()
 print(f'true_divide: 25 / 3 =', true_divide(25, 3))
 print(f'fake_divide: 25 / 3 =', fake_divide(25, 3))
 print()
